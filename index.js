80950e1b71342fb1b0c2a5d16e7b74c5224a2328
Carlos Ortube <cool3000bo@gmail.com>
2018-12-12 22:33:25 -0400

    Se concluye libreria validando el tipo de ingreso si es cadena o vector y tambien se define el tipo de salida cadena o vector indicando el valor para la respuesta

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
 'use strict';
 
 function ResolverSudoku() {
-    function solver(cad) {
-        cad = convertVector(cad);
-        var testGrid = cad;
-        var myFunction = function R(a, i, j, m, g) {
-            for (i = 80; a[i]; i-- || A)
-            ;
-            for (m = 10; g = a[i] = --m; g && R(a))
-                for (j in a)
-                    g *= j == i || a[j] ^ m || i % 9 ^ j % 9 && i / 9 ^ j / 9 && i / 27 ^ j / 27 | i % 9 / 3 ^ j % 9 / 3
-        };
-        try {
-            myFunction(testGrid);
-        } catch (e) {
-            return testGrid;
-        }
-    }
 
     function convertVector(vector) {
-        var separador = "";
-        var arregloDeSubCadenas = vector.split(separador);
+        var arregloDeSubCadenas = vector.split("");
         return arregloDeSubCadenas.map(Number);
     }
 
-    function solution(cad) {
-        cad = convertVector(cad);
+    function solution(cad, type) {
+        var option = type || "";
+        if (option == "")
+            cad = convertVector(cad);
         var testGrid = cad;
         var myFunction = function R(a, i, j, m, g) {
             for (i = 80; a[i]; i-- || A)
@@ -41,15 +26,43 @@ function ResolverSudoku() {
         }
     }
     this.solve = function (cad, type) {
-        var resp = solution(cad);
-        if (type == 'cadena') {
-         return resp.join("");
-        } else if (type == 'vector') {
-            return resp;
-        }else {
-            return "Opcion invalida! "
-        }
+        var option = type || "";
+        if (is_string(cad)) {
+            var size = cad.length;
+            if (size == 81) {
+                var resp = solution(cad, "");
+                if(option == "cadena")
+                return resp.join("");
+                else if(option == "vector")
+                return resp;
+                else 
+                return resp.join("");
+            } else {
+                return "El tamano de la cadena no contiene 81 numeros";
+            }
 
+        } else {
+            if (Array.isArray(cad)) {
+                var size = cad.length;
+                if (size == 81) {
+                    var resp = solution(cad, "vector");
+                    if(option == "cadena")
+                    return resp.join("");
+                    else if(option == "vector")
+                    return resp;
+                    else 
+                    return resp;
+                 } else {
+                    return "El tamano del vector no tiene 81 posiciones";
+                }
+            }
+        }
+    }
+
+
+
+    function is_string(x) {
+        return Object.prototype.toString.call(x) === "[object String]"
     }
 }
 if (typeof exports !== 'undefined') {
